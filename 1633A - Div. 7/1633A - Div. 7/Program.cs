936fe07aaa7fb1446b52aa34d3726f9ab7b1d9c9
Salah Eddine Nourelayne <salaheddine.nourelayne@villyz.fr>
2022-02-11 23:25:48 +0100

    Correct Output

---
+++ 1633A - Div. 7/1633A - Div. 7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1633A___Div._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int rest;
+            int lastDigit;
+            int result;
+
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (var i = 0; i < t; i++)
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                rest = n % 7;
+                lastDigit = n % 10;
+
+                if (rest <= lastDigit)
+                {
+                    result = n - rest;
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    result = n + (7 - rest);
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
