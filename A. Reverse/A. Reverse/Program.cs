745b85c8a83d7e9b4955e75ab0841e34a798ca9c
Nourelayne <nourelaynesalaheddine@gmail.com>
2022-02-22 23:11:02 +0100

    Resolving...

---
+++ A. Reverse/A. Reverse/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace A._Reverse
+{
+    class Program
+    {
+        static void RearrengeArray(int[] arr, int l, int r)
+        {
+            int c ;
+            for (int j = l; j <= r; j++)
+            {
+                Console.WriteLine(arr[j] + " " + arr[r - (j - l)]);
+                c = arr[j];
+                arr[j] = arr[r - (j - l)];
+                arr[r - (j - l)] = c;
+                Console.WriteLine(arr[j] + " " + arr[r - (j - l)]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            while (t > 0)
+            {
+                int length = Convert.ToInt32(Console.ReadLine());
+                int[] permutation = Array.ConvertAll(Console.ReadLine().Split(" "), s => int.Parse(s));
+                for(int i = 0; i < length; i++)
+                {
+                    if(permutation[i] != i + 1)
+                    {
+                        int index = Array.IndexOf(permutation, i + 1);
+                        RearrengeArray(permutation, i, index);
+                        break;
+                    }
+                }
+                
+                foreach(int i in permutation)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+                t--;
+            }
+        }
+    }
+}
