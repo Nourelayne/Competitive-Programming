bb1f6d331eb5e4b602753bb00eb68479d5d395b7
Nourelayne <nourelaynesalaheddine@gmail.com>
2022-03-11 21:56:49 +0100

    Problem Solved !

---
+++ LeetCode - 2 Sum Problem/LeetCode - 2 Sum Problem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode___2_Sum_Problem
+{
+    class Program
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[] { -1, -1 };
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            int[] result = TwoSum(nums, 9);
+
+            foreach(int e in result)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ LeetCode - 3 Sum Problem/LeetCode - 3 Sum Problem/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode___3_Sum_Problem
+{
+    class Program
+    {
+            public static IList<IList<int>> ThreeSum(int[] nums)
+            {
+                List<IList<int>> list = new List<IList<int>>();
+                int prevI = 0;
+                int prevJ = 0;
+                int rest = 0;
+                int lastNumber = 0;
+                Array.Sort(nums);
+                for (int i = nums.Length - 1; i > 1; i--)
+                {
+                    if (i != nums.Length - 1 && nums[i] == prevI)
+                    {
+                        continue;
+                    }
+                    rest = -nums[i];
+                    prevI = nums[i];
+                    prevJ = 0;
+                    for (int j = i - 1; j > 0; j--)
+                    {
+                        if (j != i - 1 && nums[j] == prevJ)
+                        {
+                            continue;
+                        }
+                        lastNumber = rest - nums[j];
+                        prevJ = nums[j];
+                        if (Array.BinarySearch<int>(nums, 0, j, lastNumber) >= 0)
+                        {
+                            list.Add(new List<int>() { nums[i], nums[j], lastNumber });
+                        }
+                    }
+                }
+                return list;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { -1, 0, 1, 2, -1, -4 };
+            IList<IList<int>> solution = ThreeSum(nums);
+
+            foreach(IList<int> list in solution)
+            {
+                foreach (int e in list)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+    }
+}
